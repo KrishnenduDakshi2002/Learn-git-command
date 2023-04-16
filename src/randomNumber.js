271eeda6c6fc0faed9d88fa0d85eb356384191c1
KrishnenduDakshi <kdakshi2018@gmail.com>
2023-04-16 22:58:56 +0530

    second random number function

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,5 +1,6 @@
-const randomNumber = (min, max) => (
-  Math.floor(Math.random() * (max - min + 1)) + min
-);
+const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
+const secondRandomNUmber = Math.floor(Math.random() * 1 + 1);
+console.log(secondRandomNUmber);
 export default randomNumber;
