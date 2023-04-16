c14d96ea6b6432f243a8ca34b0eaa5a57b9f0b40
KrishnenduDakshi <kdakshi2018@gmail.com>
2023-04-16 23:38:47 +0530

    filter out odd numbers

--- src/feature_2.js
+++ src/feature_2.js
@@ -1,1 +1,4 @@
 console.log('this is feature 2 - krishnendu Dakshi');
+// filter out the odd numbers
+const filterOdd = (arr) => arr.filter((num) => num % 2 === 0);
+console.log(filterOdd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
